c420422ca0e5d8bcaf24e6a6dce245e6a3a959da
Landon Schlangen <landonschlangen@Landons-MacBook-Pro.local>
2020-08-26 12:09:44 -0500

    initial commit

---
+++ src/data.ts
@@ -0,0 +1,29 @@
+export const data = {
+  bio: {
+    name: "Landon Schlangen",
+    tagline: "Full-Stack Developer",
+    email: "lschlangen5@gmail.com",
+    github: "https://github.com/Landon345",
+    linkedin: "https://www.linkedin.com/in/landon-schlangen-a3989a16b",
+    objective: "To get a job at a software company",
+  },
+  skills: ["React", "Angular", "Node", "Django", "Laravel", "Firebase", "SQL"],
+  positions: [
+    {
+      id: "1",
+      title: "Intern",
+      company: "Bushel",
+      startDate: "2020-05-18",
+      endDate: "2020-08-23",
+      employmentType: "FULL_TIME",
+      location: "Fargo, ND",
+      achievements: [
+        "Learned React very well",
+        "Learned how to write clean code for a large scale project",
+        "Learned Laravel very well",
+        "Learned how to use git effectively and collaborate on projects",
+        "Helped develop production ready code for one of Bushel's internal tools",
+      ],
+    },
+  ],
+};
---
+++ src/interfaces.ts
@@ -0,0 +1,18 @@
+export interface BioInterface {
+  name: string;
+  tagline: string;
+  email: string;
+  github: string;
+  linkedin: string;
+  objective: string;
+}
+
+export interface PositionInterface {
+  id: string;
+  title: string;
+  company: string;
+  startDate: string;
+  endDate?: string;
+  location: string;
+  achievements: string[];
+}
